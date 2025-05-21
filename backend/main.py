b0344673e2f9637fb3306d214749fcae21f13f3f
Joel Kong <joelkong2001@gmail.com>
2025-05-21 11:12:56 +0800

    docker half done

--- backend/main.py
+++ backend/main.py
@@ -4,12 +4,14 @@ from dotenv import load_dotenv
 
 # need rmb set flask env in docker
 env = os.getenv('FLASK_ENV', 'development')
+debug = True
 if env == 'production':
     load_dotenv(os.path.join(os.path.dirname(__file__), '.env.production'))
+    debug = False
 else:
     load_dotenv(os.path.join(os.path.dirname(__file__), '.env.development'))
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=debug)
