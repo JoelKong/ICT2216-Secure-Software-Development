5de34a777507e6f12c4347d1b17c8dca4a330903
Joel Kong <joelkong2001@gmail.com>
2025-05-28 11:57:59 +0800

    zz

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -4,17 +4,13 @@ import pluginReact from "eslint-plugin-react";
 import pluginJest from "eslint-plugin-jest";
 
 export default [
+  js.configs.recommended,
   {
     files: ["**/*.{js,mjs,cjs,jsx}"],
     plugins: {
       react: pluginReact,
       jest: pluginJest,
     },
-    extends: [
-      "eslint:recommended",
-      "plugin:react/recommended",
-      "plugin:jest/recommended",
-    ],
     languageOptions: {
       ecmaVersion: 2022,
       sourceType: "module",
@@ -22,6 +18,10 @@ export default [
         ...globals.browser,
         ...globals.jest,
       },
+    },
+    rules: {
+      ...pluginReact.configs.recommended.rules,
+      ...pluginJest.configs.recommended.rules,
     },
     settings: {
       react: {
