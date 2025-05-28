57ff19020147b1686b3252ded3a5d541006f3696
Joel Kong <joelkong2001@gmail.com>
2025-05-28 11:56:21 +0800

    pls

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -2,31 +2,31 @@ import js from "@eslint/js";
 import globals from "globals";
 import pluginReact from "eslint-plugin-react";
 import pluginJest from "eslint-plugin-jest";
-import eslint from "eslint";
-const { defineConfig } = eslint;
 
-export default defineConfig({
-  files: ["**/*.{js,mjs,cjs,jsx}"],
-  plugins: {
-    react: pluginReact,
-    jest: pluginJest,
-  },
-  extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:jest/recommended",
-  ],
-  languageOptions: {
-    ecmaVersion: 2022,
-    sourceType: "module",
-    globals: {
-      ...globals.browser,
-      ...globals.jest,
+export default [
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    plugins: {
+      react: pluginReact,
+      jest: pluginJest,
+    },
+    extends: [
+      "eslint:recommended",
+      "plugin:react/recommended",
+      "plugin:jest/recommended",
+    ],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+        ...globals.jest,
+      },
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
     },
   },
-  settings: {
-    react: {
-      version: "detect",
-    },
-  },
-});
+];
