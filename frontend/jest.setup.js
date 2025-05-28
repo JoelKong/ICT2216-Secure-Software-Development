851bf29ba45fdb58a435295c47d4c5ec4b93b102
Joel Kong <joelkong2001@gmail.com>
2025-05-28 12:23:14 +0800

    please

--- frontend/jest.setup.js
+++ frontend/jest.setup.js
@@ -2,3 +2,13 @@ import { TextEncoder, TextDecoder } from "util";
 
 global.TextEncoder = TextEncoder;
 global.TextDecoder = TextDecoder;
+
+beforeAll(() => {
+  jest.spyOn(console, "error").mockImplementation(() => {});
+  jest.spyOn(console, "log").mockImplementation(() => {});
+  jest.spyOn(console, "warn").mockImplementation(() => {});
+});
+
+afterAll(() => {
+  jest.restoreAllMocks();
+});
