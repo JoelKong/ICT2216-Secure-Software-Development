093158d22c15d6ed9a0c6f265d81595faba59a51
JoelKong <joelkong2001@gmail.com>
2025-07-04 23:51:30 +0800

    clean up

--- frontend/jest.setup.js
+++ frontend/jest.setup.js
@@ -6,10 +6,6 @@ global.TextDecoder = TextDecoder;
 beforeAll(() => {
   // Suppress console logs during tests
   jest.spyOn(console, "error").mockImplementation(() => {});
-
-  // The below 2 lines are commented out to avoid suppressing errors and warnings.
-  // jest.spyOn(console, "log").mockImplementation(() => {});
-  // jest.spyOn(console, "warn").mockImplementation(() => {});
 });
 
 afterAll(() => {
